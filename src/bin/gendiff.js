a601eb2fc60278f7c961e9bd89fbd88cd583803a
Roman <ramzesnic@gmail.com>
2019-03-04 15:02:46 +0300

    add help

---
+++ src/bin/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import commander from 'commander';
+
+commander
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-v, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
+
+
+// if (commander.format) {
+//   console.log(commander.format);
+// } else if (commander.hell) {
+//   console.log(commander.help());
+// }
