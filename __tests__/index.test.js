f5fcf284dfe153a7925cfeca117b604981246b00
Roman <ramzesnic@gmail.com>
2019-03-06 14:22:14 +0300

    fix error

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,14 +5,12 @@ const fileExtes = ['json', 'yaml', 'ini'];
 
 const runTest = (ext) => {
   it(`${ext} flat test`, () => {
-    const before = `__tests__/__fixtures__/before-flat.${ext}`;
-    const after = `__tests__/__fixtures__/after-flat.${ext}`;
+    const beforePath = `__tests__/__fixtures__/before-flat.${ext}`;
+    const afterPath = `__tests__/__fixtures__/after-flat.${ext}`;
     const expected = fs.readFileSync('__tests__/__fixtures__/expected-flat.txt', 'utf-8');
-    const actual = genDiff(before, after);
+    const actual = genDiff(beforePath, afterPath);
     expect(actual).toBe(expected);
   });
 };
 
-describe('genDiff', () => {
-  fileExtes.forEach(ext => runTest(ext));
-});
+describe('genDiff', () => fileExtes.forEach(ext => runTest(ext)));
