7ed50f6f4058a83952b4b235a8359018a309d9db
Roman <ramzesnic@gmail.com>
2019-03-06 09:31:29 +0300

    fix error

--- src/parsers.js
+++ src/parsers.js
@@ -4,6 +4,6 @@ import ini from 'ini';
 const parsers = {
   '.json': JSON.parse,
   '.yaml': yaml.safeLoad,
-  '.ini': ini.parse
+  '.ini': ini.parse,
 };
 export default (ext, data) => parsers[ext](data);
