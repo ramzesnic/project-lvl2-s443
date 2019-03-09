de0c7a1c1cfc504f51658a8efb8d14214e929620
Roman <ramzesnic@gmail.com>
2019-03-09 22:28:41 +0300

    remove abstraction

--- src/renders/plain.js
+++ src/renders/plain.js
@@ -1,15 +1,10 @@
 const stringify = value => (value instanceof Object ? '[complex value]' : value);
-const getMessage = (nodePath, nodeKey, actionPart, value1 = '', endPart = '\'\'', value2 = '') => {
-  const startPart = 'Property';
-  const message = [startPart, `'${[...nodePath, nodeKey].join('.')}'`, 'was', actionPart, `'${stringify(value1)}'`, endPart, `'${stringify(value2)}'`];
-  return message.filter(part => part !== '\'\'').join(' ');
-};
 const types = {
-  nested: (node, nodePath, iter) => `${iter(node.children, [...nodePath, node.key]).filter(item => !!item).join('\n')}`,
+  nested: (node, nodePath, iter) => `${iter(node.children, [...nodePath, node.key]).filter(item => item != null).join('\n')}`,
   unchanged: () => null,
-  added: (node, nodePath) => getMessage(nodePath, node.key, 'added with value', node.value),
-  deleted: (node, nodePath) => getMessage(nodePath, node.key, 'removed'),
-  changed: (node, nodePath) => getMessage(nodePath, node.key, 'updates. From', node.oldValue, 'to', node.newValue),
+  added: (node, nodePath) => `Property '${[...nodePath, node.key].join('.')}' was added with value '${stringify(node.value)}'`,
+  deleted: (node, nodePath) => `Property '${[...nodePath, node.key].join('.')}' was removed`,
+  changed: (node, nodePath) => `Property '${[...nodePath, node.key].join('.')}' was updates. From '${stringify(node.oldValue)}' to '${stringify(node.newValue)}'`,
 };
 
 const render = (ast) => {
